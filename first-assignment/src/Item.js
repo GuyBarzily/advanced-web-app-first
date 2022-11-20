fb973f8842cc419833c64478d102f6fe5fc81b10
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2022-11-20 11:53:33 +0200

    first component

---
+++ first-assignment/src/Item.js
@@ -0,0 +1,19 @@
+import "./Item.css";
+
+function Item() {
+  const item = {
+    name: "Milk",
+    price: "100NIS",
+    img: "https://images.immediate.co.uk/production/volatile/sites/30/2020/02/Glass-and-bottle-of-milk-fe0997a.jpg",
+  };
+
+  return (
+    <div class="expense-item">
+      <h1>{item.name}</h1>
+      <h1>{item.price}</h1>
+      <img src={item.img} height={100} width={100}></img>
+    </div>
+  );
+}
+
+export default Item;
