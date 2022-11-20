93be9929761d70bbf9acc205c9690a13bd69dde3
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2022-11-20 12:12:17 +0200

    finish first part

--- first-assignment/src/App.js
+++ first-assignment/src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
 import Item from "./Item";
 function App() {
+  const Items = [
+    {
+      name: "Milk",
+      price: "100NIS",
+      img: "https://images.immediate.co.uk/production/volatile/sites/30/2020/02/Glass-and-bottle-of-milk-fe0997a.jpg",
+    },
+    {
+      name: "Bread",
+      price: "100NIS",
+      img: "https://www.kingarthurbaking.com/sites/default/files/2020-02/the-easiest-loaf-of-bread-youll-ever-bake.jpg",
+    },
+    {
+      name: "Cheese",
+      price: "100NIS",
+      img: "https://images-prod.healthline.com/hlcmsresource/images/AN_images/healthiest-cheese-1296x728-swiss.jpg",
+    },
+  ];
+
   return (
     <div className="App">
-      <Item />
+      {/* <Item name={Items[0].name} price={Items[0].price} img={Items[0].img} /> */}
+      {Items.map((item) => {
+        return (
+          <Item
+            key={JSON.stringify(item)}
+            name={item.name}
+            price={item.price}
+            img={item.img}
+          ></Item>
+        );
+      })}
     </div>
   );
 }
--- first-assignment/src/Item.js
+++ first-assignment/src/Item.js
@@ -1,16 +1,19 @@
+import { useEffect } from "react";
 import "./Item.css";
 
-function Item() {
-  const item = {
-    name: "Milk",
-    price: "100NIS",
-    img: "https://images.immediate.co.uk/production/volatile/sites/30/2020/02/Glass-and-bottle-of-milk-fe0997a.jpg",
-  };
-
+function Item(item) {
+  //   const item = {
+  //     name: "Milk",
+  //     price: "100NIS",
+  //     img: "https://images.immediate.co.uk/production/volatile/sites/30/2020/02/Glass-and-bottle-of-milk-fe0997a.jpg",
+  //   };
   return (
-    <div class="expense-item">
-      <h1>{item.name}</h1>
-      <h1>{item.price}</h1>
+    <div className="expense-item">
+      <div>
+        <h1>{item.name}</h1>
+        <h1>{item.price}</h1>
+      </div>
+      {/* <h1>{item.price}</h1> */}
       <img src={item.img} height={100} width={100}></img>
     </div>
   );
