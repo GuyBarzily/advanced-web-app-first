21957df66f9b2a04387ff62a267c98893e2d2bd7
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2022-12-04 12:23:13 +0200

    added cart

--- first-assignment/src/Home.js
+++ first-assignment/src/Home.js
@@ -1,7 +1,13 @@
 import Item from "./Item";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Badge, Button } from "@mui/material";
+
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Home(Items) {
+  const [itemCount, setItemCount] = useState(0);
+  const [shoppingCart, setShoppingCart] = useState([]);
   const itemsArr = Items.props.sort((a, b) => {
     return a.name.localeCompare(b.name);
   });
@@ -9,6 +15,40 @@ function Home(Items) {
   const navToItem = (name) => {
     nav("/item=" + name);
   };
+  const handleAdd = (item) => {
+    setItemCount(itemCount + 1);
+
+    setShoppingCart((prev) => {
+      return [item, ...prev];
+    });
+  };
+
+  const handleRemove = (item) => {
+    setItemCount(Math.max(itemCount - 1, 0));
+    console.log(shoppingCart.indexOf(item), "item: ", item);
+    let arr = shoppingCart.splice(shoppingCart.indexOf(item), 1);
+    console.log("arr: ", arr);
+    console.log(arr);
+    setShoppingCart(arr);
+    // for (let i = 0; i < shoppingCart.length; i++) {
+    //   // console.log(shoppingCart[i]);
+    //   // console.log("i", i);
+    //   if (shoppingCart[i].name === item.name) {
+    //     console.log("i", i);
+    //     let arr = shoppingCart.splice(i, 1);
+    //     setShoppingCart(arr);
+    //   }
+    // }
+
+    // shoppingCart.forEach((i) => {
+    //   if (i === item) {
+    //     console.log(shoppingCart.indexOf(item))
+    //     let arr = shoppingCart.splice(shoppingCart.indexOf(item), 1);
+    //     setShoppingCart(arr);
+    //   }
+    // });
+  };
+
   return (
     <div
       style={{
@@ -16,12 +56,26 @@ function Home(Items) {
       }}
     >
       <h1 style={{ textAlign: "center" }}>Welcome to our SuperMarket</h1>
+      <Button
+        onClick={() => {
+          console.log(shoppingCart);
+        }}
+      >
+        Press
+      </Button>
+      <Badge color="secondary" badgeContent={itemCount}>
+        <ShoppingCartIcon />{" "}
+      </Badge>
       {itemsArr.map((item) => {
         return (
           <Item
             key={JSON.stringify(item)}
             props={item}
             onClick={navToItem}
+            // setCount={setItemCount}
+            // itemCount={itemCount}
+            handleAdd={handleAdd}
+            handleRemove={handleRemove}
           ></Item>
         );
       })}
--- first-assignment/src/Item.js
+++ first-assignment/src/Item.js
@@ -1,16 +1,37 @@
 import "./Item.css";
+import { ButtonGroup, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 function Item(items) {
   const item = items.props;
   const handleClick = () => {
     items.onClick(item.name);
   };
+  const handleCurrAdd = () => {
+    items.handleAdd(item);
+  };
+
+  const handleCurrRemove = () => {
+    items.handleRemove(item);
+  };
   return (
-    <div className="expense-item" onClick={handleClick}>
+    <div className="expense-item">
       <div>
         <h1>{item.name}</h1> <h1>{item.price + " ₪"}</h1>
       </div>
       <img src={item.img} height={100} width={100}></img>
+      <Button onClick={handleClick}>Go to item</Button>
+      <ButtonGroup>
+        <Button onClick={handleCurrRemove}>
+          {" "}
+          <RemoveIcon fontSize="small" />
+        </Button>
+        <Button onClick={handleCurrAdd}>
+          {" "}
+          <AddIcon fontSize="small" />
+        </Button>
+      </ButtonGroup>
     </div>
   );
 }
